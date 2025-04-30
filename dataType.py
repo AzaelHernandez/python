9f2161c98a9f823c9440f9e5749799d1cfa59d35
AzaelHernandez <hernandezazael9901@gmail.com>
2025-04-30 01:17:46 -0600

    dataType

---
+++ dataType.py
@@ -0,0 +1,43 @@
+#string
+print("cadena de caracteres")   
+#string va tambien con cadeas simples
+print('cadena de caracteres')
+#tripe comillas simples
+print(''' ''')
+#tripe comillas dobles
+print(""" """)
+
+#type, es para conocer el tipo de dato
+print(type("Hola mundo"))
+
+#concatenar string
+print ("Hola" + "mundo")
+
+#numero, entero
+print(8+9)
+#numero, float
+print(30.2)
+#booleano
+print(type(True))
+print(type(False))
+
+#Tipo de dato lista (List)
+print(type([10,20,30,40]))
+['Hello','Bye',"Adios"]
+[10,"Hello",True,202.22]
+[]
+#Tuples, no se pueden cambiar "inmutable"
+(10,20,30,55)
+()
+
+#Diccionarios
+print(type(
+{
+    "name":"Juan",
+    "lastname":"Perez",
+    "nickname":"nose"
+}
+))
+
+#no definido
+print(type(None))
--- holaMundo.py
+++ holaMundo.py
@@ -1,1 +1,7 @@
+#linea de texto
 print("Hola mundo")
+#esta es una suma de numero
+print(2+2)
+'''
+con esto se puede agregar un texto más largo
+'''
