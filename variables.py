5931213fb421f471a184db0d6cb8a4048f241992
AzaelHernandez <hernandezazael9901@gmail.com>
2025-04-30 01:33:50 -0600

    variables

---
+++ variables.py
@@ -0,0 +1,23 @@
+#variables {nombre simbolico para un valor}
+name = "Azael"
+age = 26
+
+x, book =100,"I Robot"
+
+#conveciones
+book_name = "I Robot" #snake case
+bookName = "Digital fortress" #camel case
+BookName = "ccdccdcdcd"# pascal case
+
+#contante, debe ir en mayúscula, no cambia
+PI=3.1416
+MY_NAME = "Maria"
+
+#la variable se puede cambiar
+book_name = "I robot"
+book_name = 121212
+
+
+
+
+
